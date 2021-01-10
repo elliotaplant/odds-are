ed25b3a032bffd85d5933306dd0d01f57a842ee0
Elliot Plant <elliotaplant@gmail.com>
2021-01-09 20:37:15 -0800

    Update the update challenge

--- functions/updateChallenge.js
+++ functions/updateChallenge.js
@@ -3,6 +3,7 @@ const { updateChallenge } = require('../utils/airtable');
 
 exports.handler = async (event) => {
   try {
+    console.log('event.body', event.body);
     const response = await updateChallenge(event.body);
     console.log('response', response);
     if (response.statusCode === 200) {
